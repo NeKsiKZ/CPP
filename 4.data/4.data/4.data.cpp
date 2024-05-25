c81e7d6ac9dc9c2b2cdb401f1acfdec4f89e3a15
NeKsiKZ <bz12330@gmail.com>
2024-05-25 17:10:48 +0200

    Nieskonczony program

---
+++ 4.data/4.data/4.data.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Data
+{
+    int day, month, year;
+
+public:
+    Data(int d = 30, int m = 11, int y = 2000)
+    {
+        day = d;
+        month = m;
+        year = y;
+    }
+    void check()
+    {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+        {
+            if ((month % 2 == 0 )&&(month > 0)&&(month <= 12)&&(day > 0)&&(day <=31))
+                cout << "zgodny miesiac i dzien" << endl;
+            else 
+                cout << "data error" << endl;
+
+            if ((month % 2 == 1) && (month > 0) && (month <= 12))
+            {
+                if (month == 2 && day > 0 && day <= 29)
+                {
+                    cout << "zgodny miesiac i dzien" << endl;
+                }
+                else if ((month % 2 == 1) && (month > 0) && (month <= 12))
+                {
+                    if (day > 0 && day <= 30)
+                    {
+                        cout << "zgodny miesiac i dzien" << endl;
+                    }
+                }
+                else 
+                    cout << "data error" << endl;
+            }
+
+
+
+
+
+        }
+        else
+        {
+            cout << "nie przestepny" << endl;
+        }
+    }
+};
+
+int main()
+{
+    Data d1;
+    d1.check();
+    return 0;
+}
