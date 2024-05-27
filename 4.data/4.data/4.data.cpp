c37a583927a9bf9084e4f9ba8689fbd00cb643ff
NeKsiKZ <bz12330@gmail.com>
2024-05-27 19:44:06 +0200

    dodaje niekompletny

--- 4.data/4.data/4.data.cpp
+++ 4.data/4.data/4.data.cpp
@@ -7,47 +7,64 @@ class Data
     int day, month, year;
 
 public:
-    Data(int d = 30, int m = 11, int y = 2000)
+    Data(int d = 31, int m = 1, int y = 2000)
     {
         day = d;
         month = m;
         year = y;
     }
-    void check()
+    int check()
     {
         if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
         {
-            if ((month % 2 == 0 )&&(month > 0)&&(month <= 12)&&(day > 0)&&(day <=31))
-                cout << "zgodny miesiac i dzien" << endl;
-            else 
-                cout << "data error" << endl;
-
-            if ((month % 2 == 1) && (month > 0) && (month <= 12))
+            if (month == 2)
             {
-                if (month == 2 && day > 0 && day <= 29)
-                {
-                    cout << "zgodny miesiac i dzien" << endl;
-                }
-                else if ((month % 2 == 1) && (month > 0) && (month <= 12))
-                {
-                    if (day > 0 && day <= 30)
-                    {
-                        cout << "zgodny miesiac i dzien" << endl;
-                    }
-                }
-                else 
-                    cout << "data error" << endl;
+                if (day > 0)
             }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+            if (month == 2 && day > 0 && day <= 29)
+            {
+                cout << "Zgodny miesiac i dzien1" << endl;
+            }
+            else if (month == 2 && day <= 0 || day > 29)
+            {
+                cout << "Data error" << endl; 
+            }
+            else if ((month % 2 == 0) && (month > 0) && (month <= 12) && (day > 0) && (day <= 31))
+            {
+                cout << "Zgodny miesiac i dzien2" << endl;
+            }
+            else if ((month % 2 == 1) && (month > 0) && (month <= 12))
+            {
+                if (day > 0 && day <= 30)
+                {
+                    cout << "Zgodny miesiac i dzien3" << endl;  
+                }
+                else 
+                    cout << "Data error33" << endl;
+            }
+
         }
         else
         {
             cout << "nie przestepny" << endl;
         }
+
+        return 0;
     }
 };
 
